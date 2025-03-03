b752803d78abb6a6859df8627f18992221b537ab
Ivan Reina Conde <>
2025-03-03 13:49:19 +0100

    Add first test for Universe and Seed initialisation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 // src/index.ts
-export function helloWorld(): string {
-  return "Hello, world!";
-};
+// export function helloWorld(): string {
+//   return "Hello, world!";
+// };
 
-console.log(helloWorld());
+// console.log(helloWorld());
+
+export class Universe {
+  cells: Cell[];
+  constructor(seed: Seed) {
+    this.cells = seed.livingCells();
+  }
+
+  livingCells(): Cell[] {
+    return [];
+  }
+}
+
+export class Seed {
+  livingCells(): Cell[] {
+    return [];
+  }
+}
+
+export class Cell {
+}
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,10 +1,24 @@
 // tests/index.test.ts
-import { helloWorld } from "../src/";
+// import { helloWorld } from "../src/";
 
-describe('helloWorld function', () => {
-  describe('in scenario A', () => {
-    it('should return a hello message', () => {
-      expect(helloWorld()).toBe('Hello, world!');
+// describe('helloWorld function', () => {
+//   describe('in scenario A', () => {
+//     it('should return a hello message', () => {
+//       expect(helloWorld()).toBe('Hello, world!');
+//     });
+//   })
+// });
+
+import { Universe, Seed } from '../src/';
+
+describe('A universe is always started with a seed that will introduce the living cells during the Universe Time.', () => {
+  describe('When the universe is created the living cells are the same as the Seed ones', () => {
+    it('Empty Seed will return empty Universe', () => {
+      const emptySeed = new Seed();
+      const emptyUniverse = new Universe(emptySeed);
+      expect(emptyUniverse.livingCells()).toStrictEqual(
+        emptySeed.livingCells()
+      );
     });
-  })
+  });
 });
